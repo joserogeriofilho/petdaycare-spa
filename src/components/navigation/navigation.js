ce50d4154d712a91e7646c55c9ae5fbc4037239c
José Rogério <roger.sama@gmail.com>
2021-05-27 21:56:36 -0300

    Estilizacao do link logout na barra de navegacao

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { logout } from '../../utils/auth'
+import styles from './navigation.module.css';
 
 
 export default function Navigation(props) {
   return (
     <div>
-      <p><Link to="/">Home</Link> | <Link to="/clients">Clients</Link> | <a href='/#/#logout' onClick={logout}>Logout</a></p>
+      <p><Link to="/">Home</Link> | <Link to="/clients">Clients</Link> | <a className={styles.logout} onClick={logout}>Logout</a></p>
     </div>
   );
 }
